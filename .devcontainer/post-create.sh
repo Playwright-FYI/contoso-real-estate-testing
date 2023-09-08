37c82f5a4b578e3348ea8980a89b5bf61f9ca9c0
Nitya Narasimhan <nitya@users.noreply.github.com>
2023-09-08 15:14:52 -0400

    reset contents

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,6 +2,3 @@
 
 ## Add any post-create commands here 
 ## Ex: any npm dependencies we want image-wide
-
-## Launch the blog app
-cd blog; npm install && npx next telemetry disable && npm run build
